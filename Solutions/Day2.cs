21383aa14f062a2c5a9b1a5e1cf3aa72726b5823
Kriyes Mahendra <kriyes@moresand.co.uk>
2024-12-03 17:45:33 +0000

    Continue day 2 part 2 fix

--- Solutions/Day2.cs
+++ Solutions/Day2.cs
@@ -16,27 +16,78 @@ namespace advent_of_code_2024
 
         private static bool ReportIsSafeWithTolerance(string report)
         {
+            int maximumDifference = 3;
+
             bool ascendingSafe = true;
             bool descendingSafe = true;
+            
+            int ascendingTolerances = 0;
+            int descendingTolerances = 0;
 
             int[] levels = ReportLevels(report);
 
             for (int i = 1; i < levels.Count() - 1; i++)
             {
-                int prevNum = levels[i - 1];
+                int previousNum = levels[i - 1];
                 int currentNum = levels[i];
                 int nextNum = levels[i + 1];
-                int currentNextDiff = Math.Abs(currentNum - nextNum);
-                int currentPrevDiff = Math.Abs(currentNum - prevNum);
-
+                
                 // ascending checks
+                {
+                    int currentMinusPrevious = currentNum - previousNum;
+                    int nextMinusPrevious = nextNum - previousNum;
+                    
+                    if (!(currentMinusPrevious > 0 && currentMinusPrevious <= maximumDifference))
+                    {
+                        if (!(nextMinusPrevious > 0 && nextMinusPrevious <= maximumDifference))
+                        {
+                            if (ascendingTolerances > 0)
+                            {
+                                int prevPrev = levels[i - 2];
+                                int currentMinusPrevPrev = currentNum - prevPrev;
 
-
+                                if (!(currentMinusPrevPrev > 0 && currentMinusPrevPrev <= maximumDifference))
+                                {
+                                    ascendingSafe = false;
+                                }
+                            }
+                        }
+                                                
+                        if (ascendingTolerances > 0)
+                        { 
+                            ascendingSafe = false;
+                        }
+                        
+                        ascendingTolerances++;
+                    }
+                }
+                
                 // descending checks
+
+                {
+                    int currentMinusNext = currentNum - nextNum;
+                    int previousMinusNext = previousNum - nextNum;
+                    
+                    if (!(currentMinusNext > 0 && currentMinusNext <= maximumDifference))
+                    {
+                        descendingTolerances++;
+
+                        if (descendingTolerances > 1)
+                        {
+                            descendingSafe = false;
+                        }
+
+                        if (!(previousMinusNext > 0 && previousMinusNext <= maximumDifference))
+                        {
+                            descendingSafe = false;
+                        }
+                    }
+                }
             }
 
-            return false;
+            return ascendingSafe || descendingSafe;
         }
+        
         public static int SafeReportCount(IEnumerable<string> reports)
         {
             int safeReports = 0;
@@ -46,6 +97,17 @@ namespace advent_of_code_2024
             }
 
             return safeReports;
+        }
+
+        public static string SafetyString(IEnumerable<string> reports)
+        {
+            string safetyString = "";
+            foreach (string report in reports)
+            {
+                safetyString += $"{ReportIsSafeWithTolerance(report)} ";
+            }
+
+            return safetyString;
         }
 
         public static int CountOfSafeReports(string[] reports)
