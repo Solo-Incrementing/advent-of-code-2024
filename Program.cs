af5a3d0ccb2fe873bbcc4eb6ebb17c78afb765fc
Mahendra, Kriyes <kriye_2003@hotmail.co.uk>
2024-12-03 04:50:48 +0000

    Move Solution classes to Solutions folder

--- Program.cs
+++ Program.cs
@@ -1,4 +1,6 @@
-﻿namespace advent_of_code_2024
+﻿using advent_of_code_2024.Solutions;
+
+namespace advent_of_code_2024
 {
     internal class Program
     {
@@ -7,10 +9,11 @@
             //Console.WriteLine(Day1.SumDifferences(InputReader.ReadAllLines(@"..\..\..\Inputs\Day1\example.txt")));
             //Console.WriteLine(Day1.SumDifferences(InputReader.ReadAllLines(@"..\..\..\Inputs\Day1\data.txt")));
             //Console.WriteLine(Day1.SumSimilarityScores(InputReader.ReadAllLines(@"..\..\..\Inputs\Day1\example.txt")));
-            //Console.WriteLine(Day1.SumSimilarityScores(InputReader.ReadAllLines(@"..\..\..\Inputs\Day1\data.txt")));
+            //Console.WriteLine(Day1.SumSimilarityScores(InputReader.ReadAllLines(@"..\..\..\Inputs\Day1\data.txt
+            //Console.WriteLine(Day2.CountOfSafeReports(InputReader.ReadAllLines(@"..\..\..\Inputs\Day2\example.txt")));
+            //Console.WriteLine(Day2.CountOfSafeReports(InputReader.ReadAllLines(@"..\..\..\Inputs\Day2\data.txt
 
-            Console.WriteLine(Day2.CountOfSafeReports(InputReader.ReadAllLines(@"..\..\..\Inputs\Day2\example.txt")));
-            Console.WriteLine(Day2.CountOfSafeReports(InputReader.ReadAllLines(@"..\..\..\Inputs\Day2\data.txt")));
+            Console.WriteLine(Day3.MethodName(InputReader.ReadAllLines(@"..\..\..\Inputs\Day2\data.txt")));
         }
     }
 }
