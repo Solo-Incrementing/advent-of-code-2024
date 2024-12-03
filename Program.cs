cc987b2152c136add30936f571192fa0b3ed0004
Mahendra, Kriyes <kriye_2003@hotmail.co.uk>
2024-12-03 07:37:50 +0000

    Update day 2 part 2 solution

--- Program.cs
+++ Program.cs
@@ -11,11 +11,14 @@ namespace advent_of_code_2024
             //Console.WriteLine(Day1.SumDifferences(InputReader.ReadAllLines(@"..\..\..\Inputs\Day1\data.txt")));
             //Console.WriteLine(Day1.SumSimilarityScores(InputReader.ReadAllLines(@"..\..\..\Inputs\Day1\example.txt")));
             //Console.WriteLine(Day1.SumSimilarityScores(InputReader.ReadAllLines(@"..\..\..\Inputs\Day1\data.txt
-            //Console.WriteLine(Day2.CountOfSafeReports(InputReader.ReadAllLines(@"..\..\..\Inputs\Day2\example.txt")));
-            //Console.WriteLine(Day2.CountOfSafeReports(InputReader.ReadAllLines(@"..\..\..\Inputs\Day2\data.txt
 
-            Console.WriteLine(Day3.SolveCorruptedMuls(InputReader.ReadAllText(@"..\..\..\Inputs\Day3\example.txt")));
-            Console.WriteLine(Day3.SolveCorruptedMuls(InputReader.ReadAllText(@"..\..\..\Inputs\Day3\data.txt")));
+            //Console.WriteLine(Day2.CountOfSafeReports(InputReader.ReadAllLines(@"..\..\..\Inputs\Day2\example.txt")));
+            //Console.WriteLine(Day2.CountOfSafeReports(InputReader.ReadAllLines(@"..\..\..\Inputs\Day2\data.txt")));
+
+            Console.WriteLine(Day2.ReportLevels(InputReader.ReadAllLines(@"..\..\..\Inputs\Day2\example.txt")[0]));
+
+            //Console.WriteLine(Day3.SolveCorruptedMuls(InputReader.ReadAllText(@"..\..\..\Inputs\Day3\example.txt")));
+            //Console.WriteLine(Day3.SolveCorruptedMuls(InputReader.ReadAllText(@"..\..\..\Inputs\Day3\data.txt")));
         }
     }
 }
--- Solutions/Day2.cs
+++ Solutions/Day2.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -8,6 +9,45 @@ namespace advent_of_code_2024
 {
     internal class Day2
     {
+        public static int[] ReportLevels(string report)
+        {
+            return report.Split(" ").Select(int.Parse).ToArray();
+        }
+
+        private static bool ReportIsSafeWithTolerance(string report)
+        {
+            bool ascendingSafe = true;
+            bool descendingSafe = true;
+
+            int[] levels = ReportLevels(report);
+
+            for (int i = 1; i < levels.Count() - 1; i++)
+            {
+                int prevNum = levels[i - 1];
+                int currentNum = levels[i];
+                int nextNum = levels[i + 1];
+                int currentNextDiff = Math.Abs(currentNum - nextNum);
+                int currentPrevDiff = Math.Abs(currentNum - prevNum);
+
+                // ascending checks
+
+
+                // descending checks
+            }
+
+            return false;
+        }
+        public static int SafeReportCount(IEnumerable<string> reports)
+        {
+            int safeReports = 0;
+            foreach (string report in reports)
+            {
+                if (ReportIsSafeWithTolerance(report)) safeReports++;
+            }
+
+            return safeReports;
+        }
+
         public static int CountOfSafeReports(string[] reports)
         {
             int safeReports = 0;
