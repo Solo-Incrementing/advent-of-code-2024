8325213a3fe423b6c9717b053c944a87eeb07bd3
Mahendra, Kriyes <kriye_2003@hotmail.co.uk>
2024-12-04 07:10:57 +0000

    Add day 4 part 2 progress

--- Solutions/Day4.cs
+++ Solutions/Day4.cs
@@ -87,5 +87,104 @@ namespace advent_of_code_2024.Solutions
 
             return searchCount;
         }
+
+        // search word must have an odd number of letters
+        public static int oddWordCrossSearch(string[] characterGrid, string searchWord)
+        {
+            if ((searchWord.Length % 2 == 0) && searchWord.Length >= 3) return -1;
+
+            int searchCount = 0;
+            Vector2[] directions = {
+                new Vector2(1, 1),
+                new Vector2(1, -1),
+                new Vector2(-1, -1),
+                new Vector2(-1, 1),
+            };
+
+            for (int row = 0; row < characterGrid.Length; row++)
+            {
+                for (int col = 0; col < characterGrid[row].Length; col++)
+                {
+                    Vector2 startPosition = new Vector2(col, row);
+
+                    if (characterGrid[col][row] == searchWord[(searchWord.Length - 1)/ 2])
+                    {
+                        bool crossFound = true;
+
+                        for (int directionIndex = 0; directionIndex < directions.Length; directionIndex++)
+                        {
+                            bool wordFound = true;
+
+                            Vector2 currentDirection = directions[directionIndex];
+
+
+                            // start from the furthest point from the center of the word
+                            // e.g if the word is 5 letters long, start from currentpos + (2,2)
+
+
+                            for (int i = 0; i < searchWord.Length; i++)
+                            {
+                                Vector2 newPosition = new Vector2(
+                                    startPosition.X + currentDirection.X * i,
+                                    startPosition.Y + currentDirection.Y * i
+                                    );
+
+                                if (
+                                    newPosition.X >= 0 &&
+                                    newPosition.X < characterGrid[row].Length &&
+                                    newPosition.Y >= 0 &&
+                                    newPosition.Y < characterGrid.Length)
+                                {
+                                    if (characterGrid[newPosition.X][newPosition.Y] != searchWord[i])
+                                    {
+                                        wordFound = false;
+                                        break;
+                                    }
+                                }
+                                else
+                                {
+                                    wordFound = false;
+                                    break;
+                                }
+                            }
+
+                            for (int i = 1; i < searchWord.Length; i++)
+                            {
+                                Vector2 newPosition = new Vector2(
+                                    startPosition.X + currentDirection.X * i,
+                                    startPosition.Y + currentDirection.Y * i
+                                    );
+
+                                if (
+                                    newPosition.X >= 0 &&
+                                    newPosition.X < characterGrid[row].Length &&
+                                    newPosition.Y >= 0 &&
+                                    newPosition.Y < characterGrid.Length)
+                                {
+                                    if (characterGrid[newPosition.X][newPosition.Y] != searchWord[i])
+                                    {
+                                        wordFound = false;
+                                        break;
+                                    }
+                                }
+                                else
+                                {
+                                    wordFound = false;
+                                    break;
+                                }
+                            }
+
+                            if (wordFound)
+                            {
+                                searchCount++;
+                            }
+                        }
+                    }
+
+                }
+            }
+
+            return searchCount;
+        }
     }
 }
