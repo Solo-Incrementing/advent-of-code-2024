5507bcb8ce14d14111ffe1371fe92fffa09e077f
Mahendra, Kriyes <kriye_2003@hotmail.co.uk>
2024-12-07 09:52:04 +0000

    Add day 4 part 2 solution

--- Program.cs
+++ Program.cs
@@ -12,18 +12,18 @@ namespace advent_of_code_2024
             //Console.WriteLine(Day1.SumSimilarityScores(InputReader.ReadAllLines(@"..\..\..\Inputs\Day1\example.txt")));
             //Console.WriteLine(Day1.SumSimilarityScores(InputReader.ReadAllLines(@"..\..\..\Inputs\Day1\data.txt
 
-            Console.WriteLine(Day2.CountSafeReports(InputReader.ReadAllLines(@"..\..\..\Inputs\Day2\example.txt")));
-            Console.WriteLine(Day2.CountSafeReports(InputReader.ReadAllLines(@"..\..\..\Inputs\Day2\data.txt")));
-            Console.WriteLine(Day2.CountSafeReportsWithTolerance(InputReader.ReadAllLines(@"..\..\..\Inputs\Day2\example.txt")));
-            Console.WriteLine(Day2.CountSafeReportsWithTolerance(InputReader.ReadAllLines(@"..\..\..\Inputs\Day2\data.txt")));
-
-            //Console.WriteLine(Day2.ReportLevels(InputReader.ReadAllLines(@"..\..\..\Inputs\Day2\example.txt")[0]));
+            //Console.WriteLine(Day2.CountSafeReports(InputReader.ReadAllLines(@"..\..\..\Inputs\Day2\example.txt")));
+            //Console.WriteLine(Day2.CountSafeReports(InputReader.ReadAllLines(@"..\..\..\Inputs\Day2\data.txt")));
+            //Console.WriteLine(Day2.CountSafeReportsWithTolerance(InputReader.ReadAllLines(@"..\..\..\Inputs\Day2\example.txt")));
+            //Console.WriteLine(Day2.CountSafeReportsWithTolerance(InputReader.ReadAllLines(@"..\..\..\Inputs\Day2\data.txt")))
 
             //Console.WriteLine(Day3.SolveCorruptedMuls(InputReader.ReadAllText(@"..\..\..\Inputs\Day3\example.txt")));
             //Console.WriteLine(Day3.SolveCorruptedMuls(InputReader.ReadAllText(@"..\..\..\Inputs\Day3\data.txt")));
 
             //Console.WriteLine(Day4.CountOfWordSearch(InputReader.ReadAllLines(@"..\..\..\Inputs\Day4\example.txt"), "XMAS"));
             //Console.WriteLine(Day4.CountOfWordSearch(InputReader.ReadAllLines(@"..\..\..\Inputs\Day4\data.txt"), "XMAS"));
+            //Console.WriteLine(Day4.oddWordCrossSearch(InputReader.ReadAllLines(@"..\..\..\Inputs\Day4\example.txt"), "MAS"));
+            //Console.WriteLine(Day4.oddWordCrossSearch(InputReader.ReadAllLines(@"..\..\..\Inputs\Day4\data.txt"), "MAS"));
 
             //var parsedSections = Day5.ParsePageOrderingRules(Day5.ParseSections(InputReader.ReadAllLines(@"..\..\..\Inputs\Day5\example.txt")).orderingRules);
 
--- Solutions/Day4.cs
+++ Solutions/Day4.cs
@@ -91,7 +91,7 @@ namespace advent_of_code_2024.Solutions
         // search word must have an odd number of letters
         public static int oddWordCrossSearch(string[] characterGrid, string searchWord)
         {
-            if ((searchWord.Length % 2 == 0) && searchWord.Length >= 3) return -1;
+            if ((searchWord.Length % 2 == 0) && searchWord.Length <= 2) return 0;
 
             int searchCount = 0;
             Vector2[] directions = {
@@ -105,22 +105,27 @@ namespace advent_of_code_2024.Solutions
             {
                 for (int col = 0; col < characterGrid[row].Length; col++)
                 {
-                    Vector2 startPosition = new Vector2(col, row);
-
                     if (characterGrid[col][row] == searchWord[(searchWord.Length - 1)/ 2])
                     {
-                        bool crossFound = true;
+                        int foundWordCount = 0;
 
                         for (int directionIndex = 0; directionIndex < directions.Length; directionIndex++)
                         {
-                            bool wordFound = true;
-
-                            Vector2 currentDirection = directions[directionIndex];
-
-
                             // start from the furthest point from the center of the word
                             // e.g if the word is 5 letters long, start from currentpos + (2,2)
 
+                            Vector2 startPosition = new Vector2(
+                                col + directions[directionIndex].X * (searchWord.Length - 1) / 2,
+                                row + directions[directionIndex].Y * (searchWord.Length - 1) / 2
+                                );
+                            
+                            bool wordFound = true;
+
+                            // currentDirection is in the opposite direction because we are going inwards from the edge of the cross
+                            Vector2 currentDirection = new Vector2(
+                                directions[directionIndex].X * -1,
+                                directions[directionIndex].Y * -1
+                                );
 
                             for (int i = 0; i < searchWord.Length; i++)
                             {
@@ -148,36 +153,12 @@ namespace advent_of_code_2024.Solutions
                                 }
                             }
 
-                            for (int i = 1; i < searchWord.Length; i++)
-                            {
-                                Vector2 newPosition = new Vector2(
-                                    startPosition.X + currentDirection.X * i,
-                                    startPosition.Y + currentDirection.Y * i
-                                    );
+                            if (wordFound) foundWordCount++;
+                        }
 
-                                if (
-                                    newPosition.X >= 0 &&
-                                    newPosition.X < characterGrid[row].Length &&
-                                    newPosition.Y >= 0 &&
-                                    newPosition.Y < characterGrid.Length)
-                                {
-                                    if (characterGrid[newPosition.X][newPosition.Y] != searchWord[i])
-                                    {
-                                        wordFound = false;
-                                        break;
-                                    }
-                                }
-                                else
-                                {
-                                    wordFound = false;
-                                    break;
-                                }
-                            }
-
-                            if (wordFound)
-                            {
-                                searchCount++;
-                            }
+                        if (foundWordCount >= 2)
+                        {
+                            searchCount++;
                         }
                     }
 
