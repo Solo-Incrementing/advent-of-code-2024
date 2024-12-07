2615a9ba5c5ed953f203cc61933b26265983c1c0
Mahendra, Kriyes <kriye_2003@hotmail.co.uk>
2024-12-07 09:04:07 +0000

    Update day 2 part 1 solution

--- Program.cs
+++ Program.cs
@@ -12,16 +12,20 @@ namespace advent_of_code_2024
             //Console.WriteLine(Day1.SumSimilarityScores(InputReader.ReadAllLines(@"..\..\..\Inputs\Day1\example.txt")));
             //Console.WriteLine(Day1.SumSimilarityScores(InputReader.ReadAllLines(@"..\..\..\Inputs\Day1\data.txt
 
-            //Console.WriteLine(Day2.CountOfSafeReports(InputReader.ReadAllLines(@"..\..\..\Inputs\Day2\example.txt")));
-            //Console.WriteLine(Day2.CountOfSafeReports(InputReader.ReadAllLines(@"..\..\..\Inputs\Day2\data.txt")));
+            Console.WriteLine(Day2.CountSafeReports(InputReader.ReadAllLines(@"..\..\..\Inputs\Day2\example.txt")));
+            Console.WriteLine(Day2.CountSafeReports(InputReader.ReadAllLines(@"..\..\..\Inputs\Day2\data.txt")));
 
             //Console.WriteLine(Day2.ReportLevels(InputReader.ReadAllLines(@"..\..\..\Inputs\Day2\example.txt")[0]));
 
             //Console.WriteLine(Day3.SolveCorruptedMuls(InputReader.ReadAllText(@"..\..\..\Inputs\Day3\example.txt")));
             //Console.WriteLine(Day3.SolveCorruptedMuls(InputReader.ReadAllText(@"..\..\..\Inputs\Day3\data.txt")));
 
-            Console.WriteLine(Day4.CountOfWordSearch(InputReader.ReadAllLines(@"..\..\..\Inputs\Day4\example.txt"), "XMAS"));
-            Console.WriteLine(Day4.CountOfWordSearch(InputReader.ReadAllLines(@"..\..\..\Inputs\Day4\data.txt"), "XMAS"));
+            //Console.WriteLine(Day4.CountOfWordSearch(InputReader.ReadAllLines(@"..\..\..\Inputs\Day4\example.txt"), "XMAS"));
+            //Console.WriteLine(Day4.CountOfWordSearch(InputReader.ReadAllLines(@"..\..\..\Inputs\Day4\data.txt"), "XMAS"));
+
+            //var parsedSections = Day5.ParsePageOrderingRules(Day5.ParseSections(InputReader.ReadAllLines(@"..\..\..\Inputs\Day5\example.txt")).orderingRules);
+
+            //Console.WriteLine(Day5.SumCorrectlyOrderedUpdates(InputReader.ReadAllLines(@"..\..\..\Inputs\Day5\example.txt")));
         }
     }
 }
--- Solutions/Day2.cs
+++ Solutions/Day2.cs
@@ -14,214 +14,48 @@ namespace advent_of_code_2024
             return report.Split(" ").Select(int.Parse).ToArray();
         }
 
-        private static bool ReportIsSafeWithTolerance(string report)
+        public static bool IsReportSafe(string report)
         {
-            int maximumDifference = 3;
-
+            int[] levels = ReportLevels(report);
             bool ascendingSafe = true;
             bool descendingSafe = true;
-            
-            int ascendingTolerances = 0;
-            int descendingTolerances = 0;
 
-            int[] levels = ReportLevels(report);
-
-            for (int i = 1; i < levels.Count() - 1; i++)
+            for (int i = 0; i < levels.Length - 1; i++)
             {
-                int previousNum = levels[i - 1];
                 int currentNum = levels[i];
                 int nextNum = levels[i + 1];
-                
-                // ascending checks
-                {
-                    int currentMinusPrevious = currentNum - previousNum;
-                    int nextMinusPrevious = nextNum - previousNum;
-                    
-                    if (!(currentMinusPrevious > 0 && currentMinusPrevious <= maximumDifference))
-                    {
-                        if (!(nextMinusPrevious > 0 && nextMinusPrevious <= maximumDifference))
-                        {
-                            if (ascendingTolerances > 0)
-                            {
-                                int prevPrev = levels[i - 2];
-                                int currentMinusPrevPrev = currentNum - prevPrev;
 
-                                if (!(currentMinusPrevPrev > 0 && currentMinusPrevPrev <= maximumDifference))
-                                {
-                                    ascendingSafe = false;
-                                }
-                            }
-                        }
-                                                
-                        if (ascendingTolerances > 0)
-                        { 
-                            ascendingSafe = false;
-                        }
-                        
-                        ascendingTolerances++;
-                    }
+                // check to falsify ascending
+                if (currentNum >= nextNum || (nextNum - currentNum) > 3)
+                {
+                    ascendingSafe = false;
                 }
-                
-                // descending checks
 
+                // check to falsify descending
+                if (currentNum <= nextNum || (currentNum - nextNum) > 3)
                 {
-                    int currentMinusNext = currentNum - nextNum;
-                    int previousMinusNext = previousNum - nextNum;
-                    
-                    if (!(currentMinusNext > 0 && currentMinusNext <= maximumDifference))
-                    {
-                        descendingTolerances++;
-
-                        if (descendingTolerances > 1)
-                        {
-                            descendingSafe = false;
-                        }
-
-                        if (!(previousMinusNext > 0 && previousMinusNext <= maximumDifference))
-                        {
-                            descendingSafe = false;
-                        }
-                    }
+                    descendingSafe = false;
                 }
             }
 
-            return ascendingSafe || descendingSafe;
+            return !(ascendingSafe && descendingSafe) && ascendingSafe || descendingSafe;
         }
-        
-        public static int SafeReportCount(IEnumerable<string> reports)
+
+        public static int CountSafeReports(string[] reports)
         {
             int safeReports = 0;
+
             foreach (string report in reports)
             {
-                if (ReportIsSafeWithTolerance(report)) safeReports++;
+                if (IsReportSafe(report)) { safeReports++; }
             }
 
             return safeReports;
         }
 
-        public static string SafetyString(IEnumerable<string> reports)
+        public static int CountSafeReportsWithTolerance(string[] reports)
         {
-            string safetyString = "";
-            foreach (string report in reports)
-            {
-                safetyString += $"{ReportIsSafeWithTolerance(report)} ";
-            }
-
-            return safetyString;
-        }
-
-        public static int CountOfSafeReports(string[] reports)
-        {
-            int safeReports = 0;
-
-            List<bool> safeReportBools = [];
-
-            foreach (string report in reports)
-            {
-                string[] levels = report.Split(" ");
-
-                bool isSafe = true;
-                bool isAscending = true;
-
-                int firstNum = int.Parse(levels[0]);
-                int secondNum = int.Parse(levels[1]);
-
-                int firstTwoNumDiff = Math.Abs(firstNum - secondNum);
-
-                if ((firstTwoNumDiff >= 1) && (firstTwoNumDiff <= 3))
-                {
-                    if (firstNum > secondNum)
-                    {
-                        isAscending = false;
-                    }
-                    else
-                    {
-                        isAscending = true;
-                    }
-                }
-                else
-                {
-                    continue;
-                }
-
-                bool dampened = false;
-
-                for (int i = 1; i < levels.Length - 1; i++)
-                {
-                    int prevNum = int.Parse(levels[i - 1]);
-                    int currentNum = int.Parse(levels[i]);
-                    int nextNum = int.Parse(levels[i+1]);
-                    int currentNextDiff = Math.Abs(currentNum - nextNum);
-                    int currentPrevDiff = Math.Abs(currentNum - prevNum);
-                    
-
-                    if (isAscending)
-                    {
-                        if ((nextNum <= currentNum) || (prevNum >= currentNum) || currentNextDiff < 1 || currentNextDiff > 3 || currentPrevDiff < 1 || currentPrevDiff > 3)
-                        {
-                            if (!dampened)
-                            {
-                                dampened = true;
-                            }
-                            else
-                            {
-                                if (prevNum > nextNum || currentNextDiff > 3)
-                                {
-                                    prevNum = int.Parse(levels[i - 2]);
-                                    currentPrevDiff = Math.Abs(currentNum - prevNum);
-
-                                    if ((prevNum > currentNum) || currentPrevDiff > 3)
-                                    {
-                                        isSafe = false;
-                                        break;
-                                    }
-                                }
-                            }
-                        }
-                    }
-                    else
-                    {
-                        if ((nextNum >= currentNum) || (prevNum <= currentNum) || currentNextDiff < 1 || currentNextDiff > 3 || currentPrevDiff < 1 || currentPrevDiff > 3)
-                        {
-                            if (!dampened)
-                            {
-                                dampened = true;
-                            }
-                            else
-                            {
-                                if (prevNum < nextNum || currentNextDiff > 3)
-                                {
-                                    prevNum = int.Parse(levels[i - 2]);
-                                    currentPrevDiff = Math.Abs(currentNum - prevNum);
-
-                                    if ((prevNum < currentNum) || currentPrevDiff > 3)
-                                    {
-                                        isSafe = false;
-                                        break;
-                                    }
-                                }
-                            }
-                        }
-                    }
-                }
-
-                if (isSafe)
-                {
-                    safeReportBools.Add(true);
-                    safeReports++;
-                }
-                else
-                {
-                    safeReportBools.Add(false);
-                }
-            }
-
-            foreach (var safeBool in safeReportBools)
-            {
-                Console.Write(safeBool + " ");
-            }
-
-            return safeReports;
+            return 0;
         }
     }
 }
